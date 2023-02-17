547a3fab62afec598e9f7af8d991e7ee4b104b3e
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-02-17 13:13:00 +0100

    stampa del paragrafo e della sua lunghezza

--- index.php
+++ index.php
@@ -7,6 +7,21 @@
     <title>Bad word replacer</title>
 </head>
 <body>
+    <?php
+        $text = "Oh, oh, luxury
+        Chidi-ching-ching could buy anything, cop that
+        Oh, oh, collard greens
+        Three degrees low, make it hot for me, drop that
+        Oh, oh, down with that shit
+        Drink this, smoke this, get down with the shit, aye
+        Oh, oh, down with the shit
+        Cop this, pop this, down with the shit";
     
+    ?>
+
+    <h1>This is the full text:</h1>
+    <P><?php echo $text ?></p>
+    <P>and its length is: <?php echo strlen($text) ?></P>
+
 </body>
 </html>

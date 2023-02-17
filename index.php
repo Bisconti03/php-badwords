7fb5652ecffcecd8d7f37ff8fb44ecd5636d38a9
giuseppe_bisconti <giuseppebisconti00@gmail.com>
2023-02-17 13:57:42 +0100

    progetto ultimato con modifica della parola da censurare con '***', e nuova stampa del testo

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bad word replacer</title>
+    <title>censored word replacer</title>
 </head>
 <body>
     <?php
@@ -16,12 +16,28 @@
         Drink this, smoke this, get down with the shit, aye
         Oh, oh, down with the shit
         Cop this, pop this, down with the shit";
+
+        $word = $_GET['censoredWord'];
+        $censoredText = str_replace("$word", "***", "$text");
+        
     
     ?>
 
     <h1>This is the full text:</h1>
     <P><?php echo $text ?></p>
     <P>and its length is: <?php echo strlen($text) ?></P>
+
+
+    <form action="" method="get">
+        <label for="selectCensoredWord">Censor a Word: </label>
+        <input type="text" name="censoredWord" id="selectCensoredWord">
+        <button>Censor!</button>
+    </form>
+
+    <h1>This is the censored text:</h1>
+    <p><?php echo $censoredText ?></p>
+    <P>and its length is: <?php echo strlen($censoredText) ?></P>
+    
 
 </body>
 </html>
